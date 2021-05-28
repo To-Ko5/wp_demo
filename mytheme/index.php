775d4710fcfd9490ea1935d127e1ea915e06df19
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-05-29 08:59:44 +0900

    Add: titleを追加

--- mytheme/index.php
+++ mytheme/index.php
@@ -5,12 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>TK Theme</title>
-    <?php wp_head() ; ?> <!-- required -->
+    <?php wp_head() ; ?>
+    <!-- required -->
   </head>
   <body>
     <div>
+      <h1><?php echo $title = the_title( ) ;?></h1>
     </div>
-
-    <?php wp_footer() ; ?> <!-- required -->
+    <?php wp_footer() ; ?>
+    <!-- required -->
   </body>
 </html>
