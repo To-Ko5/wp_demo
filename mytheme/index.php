41f03a7d035d89c9ecb5ed60f38e519251502d44
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-05-29 09:24:58 +0900

    Add: 投稿の表示

--- mytheme/index.php
+++ mytheme/index.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>TK Theme</title>
-    <?php wp_head() ; ?>
-    <!-- required -->
-  </head>
-  <body>
-    <div>
-      <h1><?php echo $title = the_title( ) ;?></h1>
-    </div>
-    <?php wp_footer() ; ?>
-    <!-- required -->
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>TK Theme</title>
+  <?php wp_head(); ?>
+  <!-- required -->
+</head>
+
+<body>
+  <div>
+    <?php if (have_posts()) : ?>
+      <h1><?php echo $title = the_title(); ?></h1>
+    <?php else : ?>
+      <h1>記事がありません</h1>
+    <?php endif; ?>
+  </div>
+  <?php wp_footer(); ?>
+  <!-- required -->
+</body>
+
 </html>
