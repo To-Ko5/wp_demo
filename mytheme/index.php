e373ce49fab48434fff5349c3f250f4c18060a65
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-05-31 07:59:53 +0900

    Move: header、footerを分割

--- mytheme/index.php
+++ mytheme/index.php
@@ -2,12 +2,7 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>TK Theme</title>
-  <?php wp_head(); ?>
-  <!-- required -->
+  <?php get_header(); ?>
 </head>
 
 <body>
@@ -29,8 +24,8 @@
       <h1>記事がありません</h1>
     <?php endif; ?>
   </div>
-  <?php wp_footer(); ?>
-  <!-- required -->
+
+  <?php get_footer(); ?>
 </body>
 
 </html>
