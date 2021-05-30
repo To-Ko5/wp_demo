45135f11edaf7b3abc22fccdd0a91fee3bb47b00
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-05-31 08:10:03 +0900

    Add: styleを追加

--- mytheme/header.php
+++ mytheme/header.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>TK Theme</title>
+<title><?php the_title(); ?></title>
+<link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
 <?php wp_head(); ?>
 <!-- required -->
