87a44c6b6512a9496db744c3c117569e2256db73
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-06-10 07:58:37 +0900

    Add: styleを追加

--- mytheme/index.php
+++ mytheme/index.php
@@ -13,10 +13,10 @@
     <?php if (have_posts()) : ?>
       <?php while (have_posts()) : the_post(); ?>
         <!-- 一覧表示 -->
-        <div>
+        <div class="card">
           <a href="<?php the_permalink(); ?>">
-            <h1><?php the_title(); ?>
-            </h1>
+            <p><?php the_title(); ?>
+            </p>
             <p><?php the_excerpt(); ?></p>
             <p><?php the_time(get_option('date_format')); ?></p>
             <p><?php the_author(); ?></p>
