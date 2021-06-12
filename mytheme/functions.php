937a4724dba0e894e6f9a435a25f90016459811b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-06-13 08:31:12 +0900

    Add: カスタム投稿にsupportsを追加

--- mytheme/functions.php
+++ mytheme/functions.php
@@ -13,6 +13,7 @@ add_action("init", function () {
     'label' => "商品",
     'public' => true,
     'menu_icon' => "dashicons-store",
+    'supports' => ['thumbnail', 'title', 'editor']
   ]);
 });
 
