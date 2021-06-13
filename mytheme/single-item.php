e0a4c5badf5185ff472bb1e9b4f68dccdc014c76
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-06-14 08:09:38 +0900

    initial commit

---
+++ mytheme/single-item.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <?php get_header(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <?php get_template_part('includes/header'); ?>
+  <p>アイテムページ</p>
+
+
+  <?php get_template_part('includes/footer'); ?>
+
+  <?php get_footer(); ?>
+</body>
+
+</html>
