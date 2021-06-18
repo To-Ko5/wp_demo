5ab54864537b1080f8b6b7d6ec25745b229f6797
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-06-19 08:39:43 +0900

    initial commit

---
+++ mytheme/archive-item.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <?php get_header(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <?php get_template_part('includes/header'); ?>
+  <div class="container">
+    <h1>アイテム一覧</h1>
+
+    <?php if (have_posts()) : ?>
+      <?php while (have_posts()) : the_post(); ?>
+        <p><?php the_title(); ?></p>
+      <?php endwhile; ?>
+    <?php endif; ?>
+  </div>
+  <?php get_template_part('includes/footer'); ?>
+
+  <?php get_footer(); ?>
+</body>
+
+</html>
