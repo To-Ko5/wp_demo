819215baaeac154c2e42d975271101a586d7468f
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-06-24 07:56:12 +0900

    Add: item表示を追加

--- mytheme/template-parts/content-item.php
+++ mytheme/template-parts/content-item.php
@@ -1,1 +1,11 @@
 <p>商品名</p>
+
+<?php $price = get_post_meta(get_the_ID(), "price", true) ?>
+<?php $author = get_post_meta(get_the_ID(), "author", true) ?>
+
+<dl>
+  <dt>価格</dt>
+  <dd><?php echo $price; ?></dd>
+  <dt>作者</dt>
+  <dd><?php echo $author; ?></dd>
+</dl>
