ba90a133ee825080be29b02b9b2230b1159cb204
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-06-26 08:27:09 +0900

    Add: if文を追加

--- mytheme/template-parts/content-item.php
+++ mytheme/template-parts/content-item.php
@@ -4,8 +4,13 @@
 <?php $author = get_post_meta(get_the_ID(), "author", true) ?>
 
 <dl>
-  <dt>価格</dt>
-  <dd><?php echo $price; ?></dd>
-  <dt>作者</dt>
-  <dd><?php echo $author; ?></dd>
+  <?php if ($price !== '') : ?>
+    <dt>価格</dt>
+    <dd><?php echo $price; ?>円</dd>
+  <?php endif; ?>
+
+  <?php if ($author !== '') : ?>
+    <dt>作者</dt>
+    <dd><?php echo $author; ?></dd>
+  <?php endif; ?>
 </dl>
