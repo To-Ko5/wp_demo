ca583390c944a133dd83bfdc9c3d2ad639231aae
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-06-30 08:00:42 +0900

    Add: viewportの変更

--- mytheme/header.php
+++ mytheme/header.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
 <title><?php the_title(); ?></title>
 <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
 <?php wp_head(); ?>
