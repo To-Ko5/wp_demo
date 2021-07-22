974e83fa9026c7d4d2fc4a249f07455cc7d20bef
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-07-22 09:30:40 +0900

    jsファイルを読み込む

--- mytheme/header.php
+++ mytheme/header.php
@@ -3,5 +3,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
 <title><?php the_title(); ?></title>
 <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
+<script src="<?php echo get_template_directory_uri(); ?>/js/index.js" defer></script>
 <?php wp_head(); ?>
 <!-- required -->
