8dcee36b5d301becc072196845cb69be438f446f
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-07-25 10:06:09 +0900

    Add: beforeunload時の処理を追加

--- mytheme/js/index.js
+++ mytheme/js/index.js
@@ -4,3 +4,8 @@ const menu = document.getElementById('menu')
 menuButton.addEventListener('click', () => {
     menu.classList.toggle('open_menu');
 })
+
+
+window.addEventListener('beforeunload', function(e){
+  menu.classList.remove('open_menu');
+});
